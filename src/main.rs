9dc16b69c9887745d2a674e0957a99fec1cafb88
Giedrius Statkevičius <giedriuswork@gmail.com>
2019-01-26 14:17:08 +0200

    main: remove verbosity option

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 // Supported input formats
-arg_enum!{
+arg_enum! {
     #[derive(PartialEq)]
     pub enum InputFormat {
         DRONE
@@ -12,7 +12,7 @@ arg_enum!{
 }
 
 // Supported output formats
-arg_enum!{
+arg_enum! {
     #[derive(PartialEq)]
     pub enum OutputFormat {
         SVG
@@ -21,41 +21,45 @@ arg_enum!{
 
 fn main() {
     let _matches = App::new("CIV - CI Pipeline Viewer")
-                        .version("0.1")
-                        .author("Giedrius Statkevičius <giedriuswork@gmail.com>")
-                        .about("Render CI pipeline files into graphical images")
-                        .arg(Arg::with_name("INPUT")
-                            .short("i")
-                            .long("input")
-                            .value_name("FILE")
-                            .help("Sets the input file to use")
-                            .required(true)
-                            .takes_value(true))
-                        .arg(Arg::with_name("INPUTFMT")
-                            .short("f")
-                            .long("ifmt")
-                            .value_name("FORMAT")
-                            .help("Sets the input format to use")
-                            .possible_values(&InputFormat::variants())
-                            .required(true))
-                        .arg(Arg::with_name("OUTPUT")
-                            .short("o")
-                            .long("output")
-                            .value_name("FILE")
-                            .help("Sets the output file to use")
-                            .required(true)
-                            .takes_value(true))
-                        .arg(Arg::with_name("OUTPUTFMT")
-                            .short("m")
-                            .long("ofmt")
-                            .value_name("FORMAT")
-                            .help("Sets the output format to use")
-                            .possible_values(&OutputFormat::variants())
-                            .required(true))
-                        .arg(Arg::with_name("v")
-                            .short("v")
-                            .multiple(true)
-                            .help("Sets the level of verbosity"))
-                        .get_matches();
+        .version("0.1")
+        .author("Giedrius Statkevičius <giedriuswork@gmail.com>")
+        .about("Render CI pipeline files into graphical images")
+        .arg(
+            Arg::with_name("INPUT")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Sets the input file to use")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUTFMT")
+                .short("f")
+                .long("ifmt")
+                .value_name("FORMAT")
+                .help("Sets the input format to use")
+                .possible_values(&InputFormat::variants())
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Sets the output file to use")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUTFMT")
+                .short("m")
+                .long("ofmt")
+                .value_name("FORMAT")
+                .help("Sets the output format to use")
+                .possible_values(&OutputFormat::variants())
+                .required(true),
+        )
+        .get_matches();
     println!("hey")
 }
